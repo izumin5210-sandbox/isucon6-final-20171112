78c78646f333784524bf6462786b0d69590640e9
agatan <agatan039@gmail.com>
2017-11-12 21:20:19 +0900

    Count

--- isucon6-final/webapp/go/src/app/app.go
+++ isucon6-final/webapp/go/src/app/app.go
@@ -198,6 +198,11 @@ func getAPIRooms(w http.ResponseWriter, r *http.Request) {
 			outputError(w, err)
 			return
 		}
+		room.StrokeCount, err = getStrokeCount(room.ID)
+		if err != nil {
+			outputError(w, err)
+			return
+		}
 		rooms = append(rooms, room)
 	}
 
